f58596ff07ea71e86a03eafa692d270aaa1d6c9f
thyagarajp <thp@adobe.com>
2024-11-11 14:09:16 +0530

    rename

--- blocks/eds-banner/eds-banner.js
+++ blocks/eds-banner/eds-banner.js
@@ -1,6 +1,7 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
+  console.log('asdas blockl', block)
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
